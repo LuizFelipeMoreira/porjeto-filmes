138d4d4b674f13b5ebfa95e200f10ee28630aa9d
unknown <luizfelipemlds65@gmail.com>
2023-08-13 12:45:19 -0300

    filtro navbar latral

--- filtro.js
+++ filtro.js
@@ -1,1 +1,9 @@
 export default function initFiltro() {}
+const filtro = document.querySelector(".filtro");
+const inputFiltro = document.querySelector(".inputFiltro");
+
+filtro.addEventListener("click", (event) => {
+  if (event.target !== inputFiltro) {
+    filtro.classList.toggle("active");
+  }
+});
